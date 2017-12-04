8f6cb56e7fc58bc908de5747615e50301aa13486
Sithum Nissanka <sithum.nissanka@gmail.com>
2017-12-04 12:09:07 +0530

    Added validation

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,15 +1,35 @@
 import { getTrailerContentForSingleMovie } from '../controllers/trailerController';
 
+const paramValidator = param => {
+  return (req, res, next) => {
+    const resourceLink = req.query[param];
+
+    if (resourceLink) {
+      const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
+
+      if (expression.test(resourceLink)) {
+        next();
+      } else {
+        res.status(400).json({status: 400, message: 'Invalid value provided for query param :resourceLink'});
+      }
+    } else {
+      res.status(400).json({status: 400, message: 'Missing required query param :resourceLink'});
+    }
+  }
+}
+
 const routes = app => {
   app.route('/trailer')
-    .get((req, res) => {
-      getTrailerContentForSingleMovie(req.query.resourceLink)
-        .then(response => {
-          res.json(response);
-        })
-        .catch(error => {
-          res.json(error.status, error);
-        })
+    .get(paramValidator('resourceLink'), getTrailerData);
+}
+
+const getTrailerData = (req, res) => {
+  getTrailerContentForSingleMovie(req.query.resourceLink)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(error => {
+      res.status(error.status).json(error);
     });
 }
 
