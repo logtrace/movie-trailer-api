975b847c892aefb4992681be5868ca9a965adbf1
Sithum Nissanka <sithum.nissanka@gmail.com>
2017-12-04 12:35:09 +0530

    Enabled caching

--- src/server.js
+++ src/server.js
@@ -8,9 +8,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Handle invalid routes
-app.use((req, res) => {
-  res.status(404).json({status: 404, message: `${req.originalUrl} not found`});
+app.use((req, res, next) => {
+  if (!req.originalUrl.includes('/trailer')) {
+    res.status(404).json({status: 404, message: `${req.originalUrl} not found`});
+  } else {
+    next();
+  }
 });
+
+// Add caching
+app.use(cache('1 day'));
 
 // Add routes to the application
 routes(app);
